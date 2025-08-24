0c4dfaf3e94d470442e491287869935707d5791f
olavo88 <olavo56@gmail.com>
2025-08-24 10:58:27 -0300

    Commit com a atualização do desafio

--- SRC/index.js
+++ SRC/index.js
@@ -1,4 +1,51 @@
 
+console.log(" ------------- Desafio Classificador de nível de Herói -------------");
+// Importa a biblioteca instalada
+const readlineSync = require('readline-sync');
 
-console.log(" ------------- Desafio de Lógica de Programação -------------");
+// Usa a função 'question' para pedir ao usuário que digite seu nome
 
+let nome = readlineSync.question("  Por favor, digite seu nome: ");
+let XP = readlineSync.question("  Digite total de pontos: ");
+// Exibe a informação coletada
+//console.log("Olá, " + nomeUsuario + "! Bem-vindo(a)!");
+
+
+//let nome = "José";
+//let XP = 1000;
+let nivel = "";
+
+if (XP <= 1000) {
+    nivel="Ferro";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+} else if(XP >= 1001 && XP <=2000  ) {
+    nivel="Bronze";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+} else if(XP >= 2001 && XP <=5000  ) {
+    nivel="Prata";
+     console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+} else if(XP >= 5001 && XP <=7000  ) {
+    nivel="Ouro";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+} else if(XP >= 7001 && XP <=8000  ) {
+    nivel="Platina";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+} else if(XP >= 8001 && XP <=9000  ) {
+    nivel="Ascendente";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+} else if(XP >= 9001 && XP <=10000  ) {
+    nivel="Imortal";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+
+} else {
+    nivel="Radiante";
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`)
+}
+
+
+console.log(" ------------- Obrigado por participar -------------");
